9b71d067028230d161957897509b3a9e0925f1a9
szg1 <63415246+szg1@users.noreply.github.com>
2022-06-18 19:31:15 +0000

    Add files via upload

--- airborne.py
+++ airborne.py
@@ -26,8 +26,10 @@ def MPU_Init():
 	bus.write_byte_data(MPU_ADR, 0x19, 7)
 	bus.write_byte_data(MPU_ADR, 0x6B, 1)
 	bus.write_byte_data(MPU_ADR, 0x1A, 0)
-	bus.write_byte_data(MPU_ADR, 0x1B, 24)
+	bus.write_byte_data(MPU_ADR, 0x1B, 28)
+	bus.write_byte_data(MPU_ADR, 0x1C, 24)
 	bus.write_byte_data(MPU_ADR, 0x38, 1)
+
 def mpurrd(addr):
 	high = bus.read_byte_data(MPU_ADR, addr)
 	low = bus.read_byte_data(MPU_ADR, addr+1)
@@ -37,9 +39,9 @@ def mpurrd(addr):
 	return val
 def mpureadall():
 	res = []
-	res.append(round(mpurrd(AXH)/16384.0,2))
-	res.append(round(mpurrd(AYH)/16384.0,2))
-	res.append(round(mpurrd(AZH)/16384.0,2))
+	res.append(round(8*mpurrd(AXH)/16384.0,2))
+	res.append(round(8*mpurrd(AYH)/16384.0,2))
+	res.append(round(8*mpurrd(AZH)/16384.0,2))
 	res.append(round(mpurrd(GXH)/131.0,2))
 	res.append(round(mpurrd(GYH)/131.0,2))
 	res.append(round(mpurrd(GZH)/131.0,2))
@@ -54,12 +56,6 @@ port = 1
 bus2 = smbus2.SMBus(port)
 calpars = bme280.load_calibration_params(bus,BME_ADR)
 
-def bmereadall():
-	data = bme280.sample(bus2,BME_ADR,calpars)
-	res = []
-	res.append(round(data.temperature,2))
-	res.append(round(data.pressure,2))
-	return(res)
 
 
 gpsp = "/dev/ttyAMA0"
@@ -87,6 +83,13 @@ def readgps(rawop=False):
 
 def senddata(text):
 	ser.write(bytes(f"{text}\n","ASCII"))
+
+def bmereadall():
+	data = bme280.sample(bus2,BME_ADR,calpars)
+	res = []
+	res.append(round(data.temperature,2))
+	res.append(round(data.pressure,2))
+	return(res)
 
 '''
 camera = PiCamera()
@@ -126,7 +129,7 @@ mpures = mpureadall()
 gpsdat = readgps()
 prept = threading.Thread(target=sender)
 
-while (mpures[1] > -1.0 and gpsdat[0] == "GPS: N/A"):
+while (mpures[1] > -1.3 and gpsdat[0] == "GPS: N/A"):
 	try:
 		mpures = mpureadall()
 	except OSError:
@@ -138,10 +141,10 @@ while (mpures[1] > -1.0 and gpsdat[0] == "GPS: N/A"):
 		prept.start()
 
 #camera.start_recording("flightrecord.h264")
-
+lasta = time_ns()
 while True:
 #	try:
-	DAT = str(time_ns()) + "\t"
+	DAT = str(time_ns()-lasta) + "\t"
 	mpures = mpureadall()
 	bmeres = bmereadall()
 	gpsdat = readgps()
@@ -151,16 +154,17 @@ while True:
 		DAT += str(i) + "\t"
 	barmes.append(bmeres[1])
 	DAT += gpsdat[1]
-	print(DAT, end = '\r')
+
+#	print(DAT, end = '\r')
 	senddata(DAT)
-	file = open("flight.log", 'a')
+	file = open("/home/LOG/flightlog.raw", 'a')
 	file.write(DAT + "\n")
 	file.close()
-	corvel = 1500 + mpures[4]*MTP
-	if corvel > 2000:
-		corvel = 2000
-	if corvel < 1000:
-		corvel = 1000
+#	corvel = 1500 + mpures[4]*MTP
+#	if corvel > 2000:
+#		corvel = 2000
+#	if corvel < 1000:
+#		corvel = 1000
 	sleep(0.5)
 '''
 	except:
